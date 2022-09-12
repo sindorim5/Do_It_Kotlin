d8e5e0836ed0c6040dccff5ab807e72c0a96528f
Kihun SONG <kihunbuzz@naver.com>
2022-09-12 12:58:05 +0900

    Initial Commit

---
+++ src/main/kotlin/chap02/section2/Valvar.kt
@@ -0,0 +1,15 @@
+package chap02.section2
+
+fun main(){
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNumber: $secondNumber")
+//    변수 이름은 숫자로 시작하면 안 된다.
+//    while, if 같은 미리 배정된 키워드는 쓸 수 없다.
+//    카멜 표기법을 사용하는 것이 좋다.
+}
